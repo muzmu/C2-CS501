124a097473c36edd3d6991bdf07f84fe0a5b0a88
myliang47 <mellaliang47@gmail.com>
2022-03-29 19:30:31 -0400

    Lazy load some of the columns for implant

--- c2/models.py
+++ c2/models.py
@@ -11,18 +11,25 @@ class Operator(db.Model):
 class Implant(db.Model):
     default_number = 120 # = arbitrary number for now
 
+    # These columns are shown for listAllImplants
     id = db.Column(db.Integer, primary_key=True)
     computer_name = db.Column(db.String(default_number))
     computer_user = db.Column(db.String(default_number))
-    computer_guid = db.Column(db.String(default_number))
     computer_privileges = db.Column(db.String(default_number)) # should be array of strings but figure out later
     connecting_ip_address = db.Column(db.String(default_number))
-    session_key = db.Column(db.String(default_number))
-    sleep = db.Column(db.String(default_number))
-    jitter = db.Column(db.String(default_number))
-    first_seen = db.Column(db.String(default_number))
     last_seen = db.Column(db.String(default_number))
     expected_check_in = db.Column(db.String(default_number))
+
+    # Deferred columns will not be loaded until they're accessed directly
+    # Once one of these columns are accessed, the rest of the columns will be loaded as well
+    # Source: https://docs.sqlalchemy.org/en/14/orm/loading_columns.html
+
+    # Not shown for listAllImplants but shown when a specific implant is queried
+    computer_guid = deferred(db.Column(db.String(default_number)), group='specific_implant')
+    session_key = deferred(db.Column(db.String(default_number)), group='specific_implant')
+    sleep = deferred(db.Column(db.String(default_number)), group='specific_implant')
+    jitter = deferred(db.Column(db.String(default_number)), group='specific_implant')
+    first_seen = deferred(db.Column(db.String(default_number)), group='specific_implant')
 
 class Command(db.Model):
     id = db.Column(db.Integer, primary_key=True)
