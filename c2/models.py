a1cb5d68d88f2598ba446a076740bcc869f1fdab
myliang47 <mellaliang47@gmail.com>
2022-03-29 19:00:05 -0400

    completed implant class in models

--- c2/models.py
+++ c2/models.py
@@ -9,7 +9,20 @@ class Operator(db.Model):
         return f'<Operator {self.username}>'
 
 class Implant(db.Model):
+    default_number = 120 # = arbitrary number for now
+
     id = db.Column(db.Integer, primary_key=True)
+    computer_name = db.Column(db.String(default_number))
+    computer_user = db.Column(db.String(default_number))
+    computer_guid = db.Column(db.String(default_number))
+    computer_privileges = db.Column(db.String(default_number)) # should be array of strings but figure out later
+    connecting_ip_address = db.Column(db.String(default_number))
+    session_key = db.Column(db.String(default_number))
+    sleep = db.Column(db.String(default_number))
+    jitter = db.Column(db.String(default_number))
+    first_seen = db.Column(db.String(default_number))
+    last_seen = db.Column(db.String(default_number))
+    expected_check_in = db.Column(db.String(default_number))
 
 class Command(db.Model):
     id = db.Column(db.Integer, primary_key=True)
