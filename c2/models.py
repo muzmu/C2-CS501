328ccd5081019bb4e40dc8afe0c6de721f70c7ec
myliang47 <mellaliang47@gmail.com>
2022-04-02 20:56:46 -0400

    Added API routes for operator

--- c2/models.py
+++ c2/models.py
@@ -1,14 +1,15 @@
-from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import deferred
 
 from .db import db
 
+
 class Operator(db.Model):
     __tablename__ = 'operator'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), unique=False, nullable=False)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(80), unique=True, nullable=False)
+    password_hash = Column(String(120), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<Operator {self.username}>'
@@ -19,40 +20,40 @@ class Implant(db.Model):
     default_number = 120 # = arbitrary number for now
 
     # These columns are shown for listAllImplants
-    id = db.Column(db.Integer, primary_key=True)
-    computer_name = db.Column(db.String(default_number), unique=False)
-    computer_user = db.Column(db.String(default_number), unique=False)
-    computer_privileges = db.Column(db.String(default_number), unique=False) # should be array of strings but figure out later
-    connecting_ip_address = db.Column(db.String(default_number), unique=False)
-    last_seen = db.Column(db.String(default_number), unique=False)
-    expected_check_in = db.Column(db.String(default_number), unique=False)
+    id = Column(Integer, primary_key=True)
+    computer_name = Column(String(default_number), unique=False)
+    computer_user = Column(String(default_number), unique=False)
+    computer_privileges = Column(String(default_number), unique=False) # should be array of strings but figure out later
+    connecting_ip_address = Column(String(default_number), unique=False)
+    last_seen = Column(String(default_number), unique=False)
+    expected_check_in = Column(String(default_number), unique=False)
 
     # Deferred columns will not be loaded until they're accessed directly
     # Once one of these columns are accessed, the rest of the columns will be loaded as well
     # Source: https://docs.sqlalchemy.org/en/14/orm/loading_columns.html
 
     # Not shown for listAllImplants but shown when a specific implant is queried
-    computer_guid = deferred(db.Column(db.String(default_number), unique=False), group='specific_implant')
-    session_key = deferred(db.Column(db.String(default_number), unique=False), group='specific_implant')
-    sleep = deferred(db.Column(db.String(default_number), unique=False), group='specific_implant')
-    jitter = deferred(db.Column(db.String(default_number), unique=False), group='specific_implant')
-    first_seen = deferred(db.Column(db.String(default_number), unique=False), group='specific_implant')
+    computer_guid = deferred(Column(String(default_number), unique=False), group='specific_implant')
+    session_key = deferred(Column(String(default_number), unique=False), group='specific_implant')
+    sleep = deferred(Column(String(default_number), unique=False), group='specific_implant')
+    jitter = deferred(Column(String(default_number), unique=False), group='specific_implant')
+    first_seen = deferred(Column(String(default_number), unique=False), group='specific_implant')
 
 class Command(db.Model):
     __tablename__ = 'command'
 
     default_number = 120 # = arbitrary number for now
 
-    id = db.Column(db.Integer, primary_key=True)
-    command_type = db.Column(db.String(default_number), unique=False)
-    command_text = db.Column(db.String(default_number), unique=False)
-    implant_id = db.Column(db.Integer, ForeignKey('implant.id'))
-    operator_id = db.Column(db.Integer, ForeignKey('operator.id'))
-    time_issued = db.Column(db.String(default_number), unique=False)
-    status = db.Column(db.String(default_number), unique=False)
-    command_result = db.Column(db.String(12000), unique=False)
+    id = Column(Integer, primary_key=True)
+    command_type = Column(String(default_number), unique=False)
+    command_text = Column(String(default_number), unique=False)
+    implant_id = Column(Integer, ForeignKey('implant.id'))
+    operator_id = Column(Integer, ForeignKey('operator.id'))
+    time_issued = Column(String(default_number), unique=False)
+    status = Column(String(default_number), unique=False)
+    command_result = Column(String(12000), unique=False)
 
 class Alert(db.Model):
     __tablename__ = 'alert'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
--- c2/operator.py
+++ c2/operator.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, jsonify, make_response,
+    redirect, render_template, request, Response, url_for
 )
+import json
 from werkzeug.exceptions import abort
 
 from .auth import login_required
@@ -9,21 +11,22 @@ from .db import db
 from .models import Command, Implant, Operator
 
 # TODO: Delete later, for testing only
-import requests
+from pprint import pprint
 
-operator = Blueprint('c2', __name__)
+bp = Blueprint('operator', __name__)
 
 
-@operator.route('/')
+@bp.route('/')
 def index():
     operators = Operator.query.all()
-    return render_template('c2/index.html', operators=operators)
+    return render_template('operator/index.html', operators=operators)
+
 
 # TODO: Maybe put operator_id in header instead
-@operator.route('/implant', methods=('GET'))
+@bp.route('/implant', methods=['GET'])
 def get_implant():
-    implant_id = request.args.get["implant_id"]
-    operator_id = request.args.get["implant_id"]
+    implant_id = request.args.get("implant_id")
+    operator_id = request.args.get("operator_id")
     error = None
 
     if not implant_id:
@@ -43,24 +46,65 @@ def get_implant():
 
     implant = Implant.query.filter_by(id=implant_id).first()
 
-    return Response(response=implant, status=200, mimetype='application/json')
+    if not implant:
+        error = "implant_id is not valid."
+        return Response(response=error, status=400, mimetype='application/json')
 
-@operator.route('/implant/all', methods=('GET'))
+    response = {
+        "computer_name": implant.computer_name,
+        "computer_privileges": implant.computer_privileges,
+        "computer_user": implant.computer_user,
+        "connecting_ip_address": implant.connecting_ip_address,
+        "expected_check_in": implant.expected_check_in,
+        "last_seen": implant.last_seen,
+        "computer_guid": implant.computer_guid,
+        "session_key": implant.session_key,
+        "sleep": implant.sleep,
+        "jitter": implant.jitter,
+        "first_seen": implant.first_seen
+    }
+
+    response = jsonify(response)
+
+    return make_response(response, 200)
+
+
+@bp.route('/implant/all', methods=['GET'])
 def list_all_implants():
     error = None
-    operator_id = request.args.get["implant_id"]
+    operator_id = request.args.get("operator_id")
 
     if not operator_id:
         error = "operator_id is required."
         return Response(response=error, status=400, mimetype='application/json')
 
-    implants = Operator.query.all()
+    operator = Operator.query.filter_by(id=operator_id).first()
 
-    return Response(response=implants, status=200, mimetype='application/json')
+    if not operator:
+        error = "operator_id is not valid."
+        return Response(response=error, status=400, mimetype='application/json')
+
+    query_result = Implant.query.all()
+    implants = []
+
+    for implant in query_result:
+        implants.append({
+            "implant_id": implant.id,
+            "computer_name": implant.computer_name,
+            "computer_privileges": implant.computer_privileges,
+            "computer_user": implant.computer_user,
+            "connecting_ip_address": implant.connecting_ip_address,
+            "expected_check_in": implant.expected_check_in,
+            "last_seen": implant.last_seen
+        })
+
+    response = jsonify(implants)
+
+    return make_response(response, 200)
 
 
 # Set request content type to application/json
-@operator.route('/storeCommandForImplant', methods=('POST'))
+@bp.route('/storeCommandForImplant', methods=['POST'])
 def store_command_for_implant():
 
     data = request.json
@@ -79,29 +123,44 @@ def store_command_for_implant():
     elif not operator_id:
         error = 'operator_id is required.';
 
+    operator = Operator.query.filter_by(id=operator_id).first()
+
+    if not operator:
+        error = "operator_id is not valid."
+        return Response(response=error, status=400, mimetype='application/json')
+
+    implant = Implant.query.filter_by(id=implant_id).first()
+
+    if not implant:
+        error = "implant_id is not valid."
+        return Response(response=error, status=400, mimetype='application/json')
+
     # TODO: Get length of timestamp, update database
     time_issued = datetime.now()
     status = "not_taken_by_implant"
     command_result = None
 
-    if error is None:
-        command = Command(
-            command_type = command_type,
-            command_text = command_text,
-            implant_id = implant_id,
-            operator_id = operator_id,
-            time_issued = time_issued,
-            status = status,
-            command_result = command_result
-        )
-        db.session.add(command)
-        db.session.commit()
-    else:
+    if error:
         return Response(response=error, status=400, mimetype='application/json')
+
+    command = Command(
+        command_type = command_type,
+        command_text = command_text,
+        implant_id = implant_id,
+        operator_id = operator_id,
+        time_issued = time_issued,
+        status = status,
+        command_result = command_result
+    )
+
+    db.session.add(command)
+    db.session.commit()
 
     return Response(response='Command stored.', status=200, mimetype='application/json')
 
 
+# For testing only, delete later
+@bp.route('/make-test-implant', methods=['GET'])
 def make_test_implant():
     id = None
 
@@ -129,10 +188,19 @@ def make_test_implant():
         id = implant.id
     except Exception as e:
         print(f"Error in make_fake_implant: {e}")
+        return make_response(e, 500)
 
-    return id
+    response = {
+        "id": id
+    }
+
+    response = jsonify(response)
+
+    return make_response(response, 200)
 
 
+# For testing only, delete later
+@bp.route('/make-test-operator', methods=['GET'])
 def make_test_operator():
     id = None
 
@@ -153,23 +221,3 @@ def make_test_operator():
         print(f"Error in make_fake_operator: {e}")
 
     return id
-
-
-# def test_get_implant():
-#     implant_id = make_test_implant()
-#     operator_id = make_test_operator()
-#
-#     print(f"implant_id = {implant_id}, operator_id = {operator_id}")
-#
-#     payload = {
-#         'implant_id': implant_id,
-#         'operator_id': operator_id
-#     }
-#
-#     result = requests.post("", data=payload)
-#     print(result)
-#
-# # Temporary testing
-# if __none__ == '__main__':
-#     # Run tests here
-#     test_get_implant()
