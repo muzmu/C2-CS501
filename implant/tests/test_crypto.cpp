24265d2595f3ecc867a43c0474523060bc43e9c6
myliang47 <mellaliang47@gmail.com>
2022-05-04 16:39:31 -0400

    Modify test_crypto

--- implant/tests/test_crypto.cpp
+++ implant/tests/test_crypto.cpp
@@ -12,51 +12,13 @@ int getLen(unsigned char* arr);
 std::string getHConnectErrorStr(unsigned int errorCode);
 
 int main() {
-  // 0 = "original_url"
-  // 1 = "username_value"
-  // 2 = "password_value"
-  // 3 = "host_key"
-  // 4 = "name"
-  // 5 = "decrypted_value"
-  // 6 = "path"
-  // 7 = "expires_utc"
-  // 8 = "source_port"
-
-  std::cout << "0: ";
-  unsigned char plaintext[] = "original_url";
+  unsigned char plaintext[] = "random string";
   encryptDecrypt(plaintext);
-  std::cout << "\n\n\n1: ";
+  std::cout << "\n\n\n";
 
-  unsigned char plaintext1[] = "username_value";
-  encryptDecrypt(plaintext1);
-  std::cout << "\n\n\n2: ";
-
-  unsigned char plaintext2[] = "password_value";
-  encryptDecrypt(plaintext2);
-  std::cout << "\n\n\n3: ";
-
-  unsigned char plaintext3[] = "host_key";
-  encryptDecrypt(plaintext3);
-  std::cout << "\n\n\n4: ";
-
-  unsigned char plaintext4[] = "name";
-  encryptDecrypt(plaintext4);
-  std::cout << "\n\n\n5: ";
-
-  unsigned char plaintext5[] = "decrypted_value";
-  encryptDecrypt(plaintext5);
-  std::cout << "\n\n\n6: ";
-
-  unsigned char plaintext6[] = "path";
-  encryptDecrypt(plaintext6);
-  std::cout << "\n\n\n7: ";
-
-  unsigned char plaintext7[] = "expires_utc";
-  encryptDecrypt(plaintext7);
-  std::cout << "\n\n\n8: ";
-
-  unsigned char plaintext8[] = "source_port";
-  encryptDecrypt(plaintext8);
+  // unsigned char plaintext1[] = "username_value";
+  // encryptDecrypt(plaintext1);
+  // std::cout << "\n\n\n";
 
 	return 0;
 }
